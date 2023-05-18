1dc070aca6c933556517e03b70e671017382798a
Lucas Muniz <lucaslima1048@gmail.com>
2023-05-18 13:03:17 -0300

    create King & Hook and placing the pieces in the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
 
 public class ChessMatch {
 	
@@ -9,6 +11,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -20,5 +23,9 @@ public class ChessMatch {
 		}
 		
 		return mat;
+	}
+	
+	public void initialSetup() {
+		board.placePiece(new King(board, Color.WHITE ), new Position(2, 1));
 	}
 }
